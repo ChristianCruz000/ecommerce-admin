ef166f9808f64f58d48adc400e86505492e09ec3
Christian <christian@simpleshape.me>
2023-10-18 00:13:30 -0400

    fix doubles

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -9,7 +9,7 @@ import { Separator } from "@/components/ui/separator";
 
 import { BillboardColumn, columns } from "./columns";
 import { DataTable } from "@/components/ui/data-table";
-import { ApiList, ApliList } from "@/components/ui/api-list";
+import { ApiList } from "@/components/ui/api-list";
 
 interface BillboardClientProps {
   data: BillboardColumn[];
